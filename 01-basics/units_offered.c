b03d7f3f0d58e425eb1f3182755904d457a72356
blessamdev <blessamuel999@gmail.com>
2026-09-21 10:01:59 +0300

    Add C program to display units offered
    This program displays a list of units offered in the semester.

---
+++ 01-basics/units_offered.c
@@ -0,0 +1,20 @@
+/*
+Author: Samuel Owino Bless
+Reg No: BCS-005-0065/2026
+Description: A simple C program that displays a list of units.
+Date: 09/09/2026
+Version: 1.0
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Units Offered This Semester:\n");
+    printf("1. Introduction to Programming\n");
+    printf("2. Data Structures\n");
+    printf("3. Computer Networks\n");
+    printf("4. Database Systems\n");
+    printf("5. Software Engineering\n");
+
+    return 0;
+}
