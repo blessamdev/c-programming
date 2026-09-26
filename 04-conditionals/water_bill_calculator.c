29bea22a3c1d41eff5708db798af3cd1187e7bdc
blessamdev <blessamuel999@gmail.com>
2026-09-26 17:26:49 +0300

    Add water bill calculator program
    This program calculates the water bill based on the number of units consumed, applying different rates for different consumption levels.

---
+++ 04-conditionals/water_bill_calculator.c
@@ -0,0 +1,35 @@
+/*
+Author: Samuel Owino Bless
+Adm No: BCS-05-0065/2026
+Date: 9.26.2026
+Description: A program that will calculate or rather acts as a water bill calculator
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	float numberofwaterunits;
+	float totalbill;
+	
+	printf("Enter the number of water units consumed.\t");
+	scanf("%f",&numberofwaterunits);
+	
+	if(numberofwaterunits >= 60){
+		totalbill = numberofwaterunits * 30;
+	}
+	else if(numberofwaterunits >= 30){
+		totalbill = numberofwaterunits * 25;
+	}
+	else
+	{
+		totalbill = numberofwaterunits * 20;
+	}
+	
+	printf("Total bill: Kshs %.2f.\n",totalbill);
+	
+	return 0;
+	
+}
+
+
